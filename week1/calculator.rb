6158ffae9a2faa72c7d10d431a2db6832cfbed97
ianknauer <ian.knauer@gmail.com>
2013-09-29 09:06:15 -0700

    First half of calculator app

---
+++ week1/calculator.rb
@@ -0,0 +1,6 @@
+puts "Hello... i am a calculator. Please give me your first number"
+num1 = gets.chomp
+puts "Excellent, now give me your second number"
+num2 = gets.chomp
+puts "Now.. What would you like to do to it? 1) add 2) subtract 3) multiply 4)divide"
+operator = gets.chomp
